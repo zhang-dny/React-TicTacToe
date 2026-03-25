0efd269cf9c2aa39c8e42bebf8a715c748713360
Danny Zhang <zhang.dny@gmail.com>
2026-03-25 16:12:06 -0400

    completed tictac toe gam

--- src/App.js
+++ src/App.js
@@ -54,56 +54,38 @@ function Board({ xIsNext, squares, onPlay}) {
     </>
 
   );
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [ a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-    }
-    return null;
-  }
 }
 
-export default function Game () {
-  const [xIsNext, setXIsNext] = useState(true);
+export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length-1];
-
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove) {
-    //todo
+    setCurrentMove(nextMove);
   }
 
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'go to move #' + move;
-    }else {
-      description = 'go to game start';
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
     }
     return (
-      <li> 
-        <button onClick={() => jumpTo(move)}> {description}</button>
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
   });
-  
+
   return (
     <div className="game">
       <div className="game-board">
@@ -113,5 +95,25 @@ export default function Game () {
         <ol>{moves}</ol>
       </div>
     </div>
-  )
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
